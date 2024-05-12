b30a0ff09f8eace37e178ea3d6f630b1246b5ba5
asif93-138 <asif93@student.sust.edu>
2024-05-12 21:23:09 +0600

    project done!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+## React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
@@ -6,3 +6,27 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# Flight Data Form
+
+To Run this project locally **Node JS** and **NPM** needed!
+
+**Setup instructions :**
+
+- Clone this repository.
+- Run the following command on the project directory to install necessary packages. `npm install`
+- Now run `npm run dev` to start local development server!
+
+This project almost meets all task requirements with smooth ui/ux. It took 16 hours to complete!
+
+[Live link](https://starlit-semifreddo-363cb7.netlify.app/)
+
+### Live link of top two front-end project
+
+- https://my-first-firebase-47b40.firebaseapp.com/
+- https://fancy-syrniki-30bcb2.netlify.app/
+
+### Link of top two github repository
+
+- https://github.com/asif93-138/summer-camp-school-client
+- https://github.com/asif93-138/toy-marketplace-client
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [flightData, setFlightData] = useState();
+  const [arr, setArr] = useState([]);
+  const [paraClass, setParaClass] = useState('d-none');
+  const [tableClass, setTableClass] = useState('d-none');
+  useEffect(() => {
+    fetch('/flightData.json')
+      .then(res => res.json())
+      .then(data => {
+        setFlightData(data); setArr(data.flightOffer);
+      })
+  }, [])
+  function displayData() {
+    setParaClass('text-start'); setTableClass("table table-striped");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='text-center container'>
+      <h3>Flight Data Form</h3>
+      <div className="btn-group mt-4">
+        <button type="button" className="btn btn-outline-primary btn-sm">Round Trip</button>
+        <button type="button" className="btn btn-outline-primary btn-sm">One Way</button>
+        <button type="button" className="btn btn-outline-primary btn-sm">Multi City</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <hr className='border-primary opacity-100' />
+      <div className='d-flex align-items-center justify-content-between'>
+        <input type="text" className="form-control border-black w-15" placeholder="LHR" />
+        <input type="text" className="form-control border-black w-15" placeholder="CDG" />
+        <input type="date" className="form-control border-black w-15" />
+        <button type='button' className='btn btn-outline-dark'><span>Day -</span> <i className="bi bi-chevron-down ms-2"></i></button>
+        <button type='button' className='btn btn-outline-dark'><span>Day +</span> <i className="bi bi-chevron-down ms-2"></i></button>
+        <button type='button' className='btn btn-outline-dark'><span>Anytime</span> <i className="bi bi-chevron-down ms-4"></i></button>
+        <span className='fs-4'>+</span>
+        <button type='button' className='btn btn-outline-dark'><span>ADT</span> <i className="bi bi-chevron-down ms-5"></i></button>
+        <button type='button' className='btn btn-outline-dark'><span>1</span>  <i className="bi bi-chevron-down ms-5"></i></button>
+        <span className='fs-4'>+</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <hr className='border-primary opacity-100' />
+      <div className='d-flex justify-content-between align-items-center'>
+        <section>
+          <input type="checkbox" className="form-check-input border-black me-2" />
+          <label className="form-check-label">Extra Options</label>
+        </section>
+        <section>
+          <span>Environment</span>
+          <input type="radio" className="form-check-input border-black mx-2" />
+          <label className="form-check-label">Dummy</label>
+          <input type="radio" className="form-check-input border-black mx-2" />
+          <label className="form-check-label">PDT</label>
+        </section>
+        <button onClick={displayData} type='button' className='btn btn-primary'>SEARCH</button>
+      </div>
+      <hr className='border-primary opacity-100' />
+      <p className={paraClass}>{flightData?.message}</p>
+      <table className={tableClass}>
+        <thead className='table-secondary'>
+          <tr>
+            <th className='text-secondary'>FLIGHT</th>
+            <th className='text-secondary'>AIRCRAFT</th>
+            <th className='text-secondary'>CLASS</th>
+            <th className='text-secondary'>FARE</th>
+            <th className='text-secondary'>ROUTE</th>
+            <th className='text-secondary'>DEPARTURE</th>
+            <th className='text-secondary'>ARRIVAL</th>
+            <th className='text-secondary'>DURATION</th>
+            <th className='text-secondary'>PRICE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {arr.map(e => (
+            <tr className='border-bottom border-danger'>
+              <td className='text-secondary'>{e.itineraries[0].segments.map(x => <p className='m-0'>{x.carrierCode} {x.aircraft}</p>)}</td>
+              <td className='text-secondary'>{e.itineraries[0].segments.map(x => <p className='m-0'>{x.flightNumber}</p>)}</td>
+              <td className='text-secondary'>{e.class[0].map(x => <p className='m-0'>{x}</p>)}</td>
+              <td className='text-secondary'>{e.fareBasis[0].map(x => <p className='m-0'>{x}</p>)}</td>
+              <td className='text-secondary'>{e.itineraries[0].segments.map(x => <p className='m-0'>{x.departure.iataCode}-{x.arrival.iataCode}</p>)}</td>
+              <td className='text-secondary'>{e.itineraries[0].segments.map(x => <p className='m-0'>{x.departure.at}</p>)}</td>
+              <td className='text-secondary'>{e.itineraries[0].segments.map(x => <p className='m-0'>{x.arrival.at}</p>)}</td>
+              <td className='text-secondary'>{e.itineraries[0].duration}</td>
+              <td className='text-secondary'><p className='m-0'>{e.price}</p><button type='button' className='btn btn-primary btn-sm'>SELECT</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
